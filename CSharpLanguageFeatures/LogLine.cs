43c1c37aafdc080e54be81302e02654200512e71
uditgandhi <coder.udit@gmail.com>
2023-04-07 09:03:36 -0400

    Updated with initial code.

---
+++ CSharpLanguageFeatures/LogLine.cs
@@ -0,0 +1,12 @@
+namespace CSharpLanguageFeatures;
+
+public static class LogLine
+{
+    //Range x..y
+    public static string Message(string logLine) =>
+        logLine[(logLine.IndexOf(':') + 1)..].Trim();
+    public static string LogLevel(string logLine) =>
+        logLine[1..logLine.IndexOf(']')].ToLower();
+    public static string Reformat(string logLine) =>
+        $"{Message(logLine)} ({LogLevel(logLine)})";
+}
